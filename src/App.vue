8a336286373bd474b06df2b7825434740963273f
jaehyun <0908jhk@naver.com>
2026-03-27 14:25:58 +0900

    [#2-1] 포커 가장 높은 족보 강조 표시 및 하트 다이아 빨간색 표시

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<template>
+  <div>
+    <h2>내 카드</h2>
+    <CardList :cards="playerCards" :highlightCards="bestCards" />
+
+    <h2>보드</h2>
+    <CardList :cards="boardCards" :highlightCards="bestCards" />
+
+    <HandResult :hand="bestHand" />
+  </div>
+</template>
+
 <script setup>
 import { onMounted } from 'vue';
 import { usePoker } from './composables/usePoker';
@@ -5,21 +17,9 @@ import { usePoker } from './composables/usePoker';
 import CardList from './components/CardList.vue';
 import HandResult from './components/HandResult.vue';
 
-const { playerCards, boardCards, bestHand, init } = usePoker();
+const { playerCards, boardCards, bestHand, bestCards, init } = usePoker();
 
 onMounted(() => {
   init();
 });
 </script>
-
-<template>
-  <div>
-    <h2>보드</h2>
-    <CardList :cards="boardCards" />
-
-    <h2>내 카드</h2>
-    <CardList :cards="playerCards" />
-
-    <HandResult :hand="bestHand" />
-  </div>
-</template>
